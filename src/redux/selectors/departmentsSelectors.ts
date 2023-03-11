19f485d6644859b7492d6afd0d5cf34ba293c668
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2023-03-11 18:55:57 +0200

    feat: Update redux logic of departments
    1. Update departmentsServiceApi.ts
    2. Update departmentsSlice.ts
    3. Update departmentsSelectors.ts
    4. Update departmentsOperations.ts
    5.  Update redux.types.ts

--- src/redux/selectors/departmentsSelectors.ts
+++ src/redux/selectors/departmentsSelectors.ts
@@ -1,11 +1,40 @@
+import { SelectValuesTypes } from "../../types/departments-components.types";
 import { IState } from "../../types/redux.types";
 import { ObjectData } from "../../types/redux.types";
 
-export const departmentsDataSelector = (state: IState): [] | ObjectData =>
-  state.departmentsState.departmentsData.data;
-
+// All cities
 export const departmentsCitiesDataSelector = (state: IState): [] | ObjectData =>
   state.departmentsState.citiesData.data;
 
-export const departmentsSelectSelector = (state: IState): boolean =>
+// Get departments for city
+export const departmentsDataSelector = (state: IState): [] | ObjectData =>
+  state.departmentsState.departmentsData.data;
+
+// Total count for pagination
+export const departmentsTotalCountSelector = (
+  state: IState
+): {
+  totalCount: number;
+} => state.departmentsState.departmentsData.info;
+
+// Loading status
+export const departmentsLoadingSelector = (state: IState): boolean =>
   state.departmentsState.loading;
+
+// Page value
+export const departmentsPageSelector = (state: IState): number =>
+  state.departmentsState.page;
+
+// Ref value for find and render one card with dpt
+export const departmentsDepartmentRefSelector = (
+  state: IState
+): string | undefined => state.departmentsState.departmentRef;
+
+// Dpt select value, need for refetching, and choosing value
+export const departmentsSelectValueSelector = (
+  state: IState
+): SelectValuesTypes => state.departmentsState.departmentsSelectValue;
+
+// Get city for fetch data with pagination, and for this city
+export const departmentsCitySelector = (state: IState): string =>
+  state.departmentsState.city;
--- src/services/api/departmentsServiceApi.ts
+++ src/services/api/departmentsServiceApi.ts
@@ -4,7 +4,7 @@ import { IDataFromApi } from "../../types/redux.types";
 const { REACT_APP_NP_API_KEY, REACT_APP_NP_BASE_URL = "" } = process.env;
 
 export const departmentsCitiesServiceApi = async (): Promise<
-  AxiosResponse<IDataFromApi>
+  AxiosResponse<IDataFromApi<{ totalCount: number }>>
 > => {
   const response = await axios.post(REACT_APP_NP_BASE_URL, {
     apiKey: REACT_APP_NP_API_KEY,
@@ -16,15 +16,21 @@ export const departmentsCitiesServiceApi = async (): Promise<
   return response;
 };
 
-export const departmentsServiceApi = async (
-  city: string
-): Promise<AxiosResponse<IDataFromApi>> => {
+export const departmentsServiceApi = async ({
+  city,
+  page,
+}: {
+  city: string;
+  page: number;
+}): Promise<AxiosResponse<IDataFromApi<{ totalCount: number }>>> => {
   const response = await axios.post(REACT_APP_NP_BASE_URL, {
     apiKey: REACT_APP_NP_API_KEY,
     modelName: "Address",
     calledMethod: "getWarehouses",
     methodProperties: {
       CityName: city,
+      Page: page,
+      Limit: "12",
     },
   });
 
